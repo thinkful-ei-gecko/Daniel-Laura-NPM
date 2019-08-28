80f60a1ea73c1e1c1149e76bf4ed79a97d0814cf
Daniel <herrnichols@gmail.com>
2019-08-28 14:40:35 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-'use strict';
-/* global shoppingList, store, api */
+
+import $ from 'jquery';
+import shoppingList from './shopping-list.js';
+import api from './api.js';
+import store from './store.js';
+import './index.css';
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
-
   // On initial load, fetch Shopping Items and render
   api.getItems()
     .then((items) => {
